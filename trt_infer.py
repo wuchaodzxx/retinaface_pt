c7152c74d2ffc73a9e142e796e44ebc730e36971
Your Name <you@example.com>
2020-02-13 10:48:57 +0800

    add

--- trt_infer.py
+++ trt_infer.py
@@ -4,4 +4,9 @@ test on tensorrt engine
 using converted trt engine 
 to see if that model works or not
 
+
+abandont!! pls look for our C++ version on MANA platform:
+
+http://manaai.cn/aicodes_detail3.html?id=46
+
 """
